c98aa8dd4629342bee29593864f868050dccd81a
Tayfun Karakavuz <114873489+tayfunka@users.noreply.github.com>
2024-12-17 17:59:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+  return {
+    memberSkills: [
+      {
+        name: 'HTML',
+        level: 'Intermediate',
+        experience: '2 years'
+      },
+      {
+        name: 'CSS',
+        level: 'Intermediate',
+        experience: '2 years'
+      },
+      {
+        name: 'JavaScript',
+        level: 'Intermediate',
+        experience: '2 years'
+      },
+      {
+        name: 'React',
+        level: 'Intermediate',
+        experience: '2 years'
+      }
+    ]
+  }
+}
